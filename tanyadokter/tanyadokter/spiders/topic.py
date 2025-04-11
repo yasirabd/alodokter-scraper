2c4a1c20dba75eb097dbcd67206ae2834e4866c9
yasirabd <sir.yasirabd@gmail.com>
2025-04-12 03:22:39 +0700

    feat: add topik spider

---
+++ tanyadokter/tanyadokter/spiders/topic.py
@@ -0,0 +1,27 @@
+import html
+import json
+from bs4 import BeautifulSoup
+import scrapy
+from tanyadokter.items import TopicItem
+
+
+class TopicSpider(scrapy.Spider):
+    name = "topic"
+    allowed_domains = ["alodokter.com"]
+    start_urls = ["https://www.alodokter.com/komunitas/topik#"]
+
+    def parse(self, response):
+        raw_json = response.xpath('//search-a-z-topic/@search-results').get()
+        if raw_json:
+            decoded_json = html.unescape(raw_json)
+            try:
+                topics = json.loads(decoded_json)
+                for topic in topics:
+                    item = TopicItem()
+                    item['topic'] = topic.get('post_title')
+                    item['url'] = response.urljoin(f"/komunitas/topic-tag/{topic.get('permalink')}")
+                    yield item
+            except json.JSONDecodeError:
+                self.logger.error("Gagal memuat JSON.")
+        else:
+            self.logger.error("Atribut 'search-results' tidak ditemukan.")
